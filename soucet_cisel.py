e273fbd139675781fb3affd4114f685dbf94b459
marek33adam <152216993+marek33adam@users.noreply.github.com>
2024-01-20 16:10:53 +0100

    Add files via upload

---
+++ soucet_cisel.py
@@ -0,0 +1,9 @@
+# Získání vstupu od uživatele
+cislo1 = float(input("Zadejte první číslo: "))
+cislo2 = float(input("Zadejte druhé číslo: "))
+
+# Výpočet součtu
+soucet = cislo1 + cislo2
+
+# Výpis výsledku
+print(f"Součet {cislo1} a {cislo2} je: {soucet}")
